e27a23f512a4f67c38038a7c6327b13786ed8837
Zach Berman <zberman2023@kehillahstudent.org>
2022-11-08 10:36:59 -0800

    docs

--- src/Graph.java
+++ src/Graph.java
@@ -6,8 +6,10 @@ public class Graph {
   private Node[] nodeList;
 
   /**
-  * Graph constructor turns a Set of edges and nodes into an Adjacency Matrix graph
+  * Graph constructor turns a Set of edges and nodes into an Adjacency Matrix graph, with Node Id's on its axis
+  * also creates a Node array that corresponds to the order of the Matrix axis
   * @param nodes the Set of nodes to be in the Graph
+  * @param edges the Set of edges to connect the nodes in the graph
   **/
   public Graph(Set<Node> nodes, Set<Edge> edges) {
 
@@ -17,6 +19,7 @@ public class Graph {
       size = Math.max(size, n.getId());
     }
 
+    //create ordered array 
     nodeList = new Node[size+1];
     for (Node n : nodes) {
       nodeList[n.getId()] = n;
@@ -24,12 +27,18 @@ public class Graph {
 
     Matrix = new boolean[size+1][size+1];
 
-    // start node Ids correspond to Matrix rows, end node Ids to Matrix col
+    // start node Ids correspond to Matrix rows, end node Ids to Matrix cols
     for (Edge e : edges)
       Matrix[e.getStart().getId()][e.getEnd().getId()] = true;
 
   }
 
+  /**
+   * Finds Nodes that are adjacent to a given node
+   * Adjacent means that an edge points to the node, not vice versa
+   * @param id the Id of the node to find the adjacent nodes of
+   * @return A Node Set of the adjacent nodes
+   */
   public Set<Node> adjacentNodes(int id) {
     Set<Node> out = new HashSet<Node>();
     for(int c = 0; c < Matrix[id].length; c++) {
@@ -48,11 +57,5 @@ public class Graph {
       }
     }
     return out;
-  }
-}
-
-class SortById implements Comparator<Node> {
-  public int compare(Node a, Node b) {
-    return a.getId() - b.getId();
   }
 }
